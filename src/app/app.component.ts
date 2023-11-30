a75dae7f97baa15a1153891896040c9aa52c27cf
Gilberto <gilbertbv95@gmail.com>
2023-11-30 14:54:27 -0500

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { inject, Component, OnInit } from '@angular/core';
+import { DEFAULT_INTERRUPTSOURCES, Idle } from '@ng-idle/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'idle-example';
+export class AppComponent implements OnInit {
+    title = 'idle-example';
+    private idle = inject(Idle);
+
+    constructor() {
+        this.idle.setIdle(5)
+        this.idle.setTimeout(1000)
+        this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES)
+
+        this.idle.onIdleStart.subscribe(() => {
+            console.log('empezaste a vaguear');
+        })
+
+        this.idle.onIdleEnd.subscribe(() => {
+            console.log('empezaste a trabajar')
+        })
+
+        this.idle.onTimeoutWarning.subscribe(() => {
+            console.log('casi te vas a salir')
+        })
+
+        this.idle.onTimeout.subscribe(() => {
+            this.idle.stop();
+            console.log('saliste men')
+        })
+    }
+
+    ngOnInit() {
+        this.idle.watch()
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        NgIdleKeepaliveModule.forRoot(),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
